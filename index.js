02b448ddeff2bdec2cacd15e9abf3d2b3f629a95
narekpedanyan <narek@corpora.us>
2024-06-08 21:59:31 +0400

    Module

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('>>>');
+const myModule = (() => {
+    const privateFoo = () => {}
+    const privateBar = []
+    const exported = {
+        publicFoo: () => {},
+        publicBar: () => {}
+    }
+    return exported
+})();
+
+console.log(myModule, 'myModule');
+console.log(myModule.privateFoo, myModule.privateBar);
+
