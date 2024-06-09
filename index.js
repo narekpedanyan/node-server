9bdcf60f9fce64abf02adaba8f4673ce9596586c
narekpedanyan <narek@corpora.us>
2024-06-09 19:02:05 +0400

    Homework 1 implemented!

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const myModule = (() => {
-    const privateFoo = () => {}
-    const privateBar = []
-    const exported = {
-        publicFoo: () => {},
-        publicBar: () => {}
-    }
-    return exported
-})();
+const http = require('http');
 
-console.log(myModule, 'myModule');
-console.log(myModule.privateFoo, myModule.privateBar);
+console.log(http, 'http');
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
